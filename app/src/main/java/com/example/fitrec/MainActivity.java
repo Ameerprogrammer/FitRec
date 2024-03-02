de332faabb848db59d67d04f0188490f4804a12f
Ameer <47337834+AmeerAhmedprogrammer@users.noreply.github.com>
2024-03-02 00:34:49 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/fitrec/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.fitrec;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+    }
+}

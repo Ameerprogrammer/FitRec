00f1ea0405222bee54c93634e47342d60ad0561c
Ameer <47337834+ameerahmedprogrammer@users.noreply.github.com>
2024-03-02 00:34:49 -0500

    Added a Sign Up screen, tweaked the home screen a little bit

--- app/src/main/java/com/example/fitrec/MainActivity.java
+++ app/src/main/java/com/example/fitrec/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.fitrec;
 
+import android.content.Intent;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
+    }
+
+    public void launchSignUp(View v) {
+        //launch a new screen
+
+        Intent i = new Intent(this, SignUpScreen.class);
+        startActivity(i);
     }
 }

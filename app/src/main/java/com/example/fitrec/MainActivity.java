32789b530960eea6b0795d8206dd551f1d9fa187
Ameer <47337834+AmeerAhmedprogrammer@users.noreply.github.com>
2024-04-14 23:37:15 -0400

    Figured how to do the constraints and positioning, edited main screen to make it look pretty

--- app/src/main/java/com/example/fitrec/MainActivity.java
+++ app/src/main/java/com/example/fitrec/MainActivity.java
@@ -17,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void launchSignUp(View v) {
         //launch a new screen
-
         Intent i = new Intent(this, SignUpScreen.class);
         startActivity(i);
     }

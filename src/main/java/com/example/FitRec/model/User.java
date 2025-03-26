ab7dbfa2c4ef52052e5d446e989c1c083f98f169
Ameer <ameermgmt@gmail.com>
2025-03-25 23:31:58 -0400

    test in intelij
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/example/FitRec/model/User.java
@@ -0,0 +1,21 @@
+package com.example.FitRec.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private int age;
+}

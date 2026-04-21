3968c1999507e3a29698cd3e4f27f7e63e38edb4
Ameer Ahmed <aahmed402@gatech.edu>
2026-04-20 20:51:31 -0400

    deleted backend stuff to keep the backend separate now

--- src/main/java/com/example/FitRec/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.FitRec.repository;
-
-import com.example.FitRec.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
